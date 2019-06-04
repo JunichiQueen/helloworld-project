aacc7054d1e85f208b6d545b780d9958c1270e1a
Harry <harry-buck@hotmail.co.uk>
2019-06-04 15:08:39 +0100

    Fixed repo

---
+++ helloworld application/src/helloworld/JavaMap.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+import java.util.HashMap;
+
+public class JavaMap {
+	
+	
+	
+	public static Object takeMap(HashMap<Integer, Object> map1, int key) {
+		return map1.get(key);
+	}
+
+}
